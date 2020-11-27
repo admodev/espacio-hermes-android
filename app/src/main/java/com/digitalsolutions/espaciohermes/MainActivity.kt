cecbaf7f816ac2a761b8bcc15852e0e4cef4fb52
admodev <admosdigital@yahoo.com>
2020-11-27 01:07:24 -0300

    agregar firebase, agregar inicio de sesion con Facebook

--- app/src/main/java/com/digitalsolutions/espaciohermes/MainActivity.kt
+++ app/src/main/java/com/digitalsolutions/espaciohermes/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.digitalsolutions.espaciohermes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
     }
 }
